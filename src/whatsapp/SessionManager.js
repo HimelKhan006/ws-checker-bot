cec9d6fc4cba4e76e4fbf74c6683e8ebbda77f0c
HimelKhan006 <himel@example.com>
2026-08-03 18:46:32 +0600

    Update_browser_identity_name_to_WS_CHECKER_PRO_BOT

--- src/whatsapp/SessionManager.js
+++ src/whatsapp/SessionManager.js
@@ -117,7 +117,7 @@ class SessionManager {
       logger,
       auth: authState,
       printQRInTerminal: false,
-      browser: Browsers.ubuntu('Chrome'),
+      browser: ['WS CHECKER PRO BOT', 'Chrome', '20.0.0'],
       syncFullHistory: false,
       generateHighQualityLinkPreview: false,
       markOnlineOnConnect: true,
